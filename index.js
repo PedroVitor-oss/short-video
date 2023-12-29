902380a7a554188a78fa3291b0a154daac8f26b9
PedroVitor-oss <pedro.santos22@escola.pr.edu.br>
2023-12-29 12:08:50 -0300

    first commit

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 const { app } = require("./src/app");
 const { createHome,createStyleHome } = require("./components/home");
 const { port } = require("./config.json");
-
+let data = require("./db/data.json")
 app.get("/",(req,res)=>{
 
     const isMobile = req.headers['user-agent'].includes("Mobile");
     
+    if(isMobile) res.redirect("/mobile");
 
     res.render("home",
     {
-        title:"app node hbs",
+        title:"short videos gallery ",
         isMobile,
-        Home:createHome("Seja Bem Vindo","abra o index.js para começar aprogramar seu projeto"),
-        htmlStyles:[
-            {css:createStyleHome(isMobile)},
+        data:JSON.stringify(data),
+        styles:[
         ],
-        stylesMoblile:[
-           
-        ]
     })
 })
 
+app.get("/project/:name",(req,res)=>{
+    const nameProject = req.params.name;
+
+    let project;
+    for(const tProject of data.projects){
+        if(tProject.title == nameProject){
+            project = tProject;
+            break;
+        }
+    }
+
+
+    res.render("project",{
+        title:nameProject + " - porject ",
+        project,
+    })
+})
+
+app.get('/mobile',(req,res)=>{
+    res.render("mobile");
+})
 app.listen(port,console.log("aberto  em https://localhost:"+port));
